434d732a48d6be6d63154f7184740ea311039a41
Patrick Dubroy <pdubroy@gmail.com>
2014-07-30 08:26:35 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,4 @@
+// Copyright (c) 2015 Patrick Dubroy <pdubroy@gmail.com>
+// This software is distributed under the terms of the MIT License.
+
+'use strict';
---
+++ test/test-joins.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var test = require('tape');
+
+test('example', function (t) {
+  t.end();
+});
