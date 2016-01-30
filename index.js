645da4804a6c74baf96f48b63c8e18c0e9ada846
Patrick Dubroy <pdubroy@gmail.com>
2016-01-30 14:33:42 +0100

    Add `spawn`, and implement blocking send.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 var assert = require('assert');
 var csp = require('js-csp');
+var inherits = require('inherits');
 
 var BUFFER_SIZE = 512;
 
@@ -46,6 +47,7 @@ Reaction.prototype.run = function() {
 
 JoinPattern.prototype.and = function(chan) {
   this._channels.push(chan);
+  return this;
 };
 
 JoinPattern.prototype.do = function(fn) {
@@ -58,6 +60,7 @@ function Channel() {
   }
   this._chan = csp.chan(BUFFER_SIZE);
   this._waiting = [];
+  this._queue = [];
 }
 
 Channel.prototype.isSynchronous = function() {
@@ -75,12 +78,25 @@ Channel.prototype.send = function(val) {
   // Run up to one reaction that is waiting on this channel.
   // TODO: Should we try to run as many as possible?
   for (var i = 0; i < this._waiting.length; ++i) {
-    var result = this._waiting[i].try();
+    var reaction = this._waiting[i];
+    var result = reaction.try();
     if (result) {
-      return result.value;
+      // If this is an asynchronous channel, don't return the result
+      // directly -- pump it into the iterator any iterator that is
+      // waiting on the synchronous channel associated with this pattern.
+      if (this.isSynchronous()) {
+        return result.value;
+      }
+      // TODO: Refactor this.
+      var chan = reaction._pattern._channels[0];
+      if (chan.isSynchronous() && chan._queue.length >= 0) {
+        var proc = chan._queue.shift();
+        proc.step(result.value);
+      }
+      return;  // eslint-disable-line consistent-return
     }
   }
-  throw new Error('FIXME - Block here');
+  return {status: 'BLOCKING', channel: this};
 };
 
 Channel.prototype._take = function() {
@@ -93,18 +109,50 @@ Channel.prototype._addReaction = function(r) {
 };
 
 function AsyncChannel() {
+  if (!(this instanceof AsyncChannel)) {
+    return new AsyncChannel();
+  }
+  Channel.call(this);
 }
+inherits(AsyncChannel, Channel);
 
 AsyncChannel.prototype.isSynchronous = function() {
   return false;
 };
 
+class Process {
+  constructor(fn, optArgs, optThisArg) {
+    assert.equal(typeof fn, 'function', 'Expected a generator function');
+    this._iter = fn.apply(optThisArg, optArgs || []);
+  }
+
+  step(value) {
+    var result = this._iter.next(value);
+    var state = this._state = result.value;
+    if (typeof state === 'object' && state.status === 'BLOCKING') {
+      state.channel._queue.push(this);
+    }
+    this._done = result.done;
+  }
+
+  isComplete() {
+    return this._done;
+  }
+}
+
 function when(chan) {
   return new JoinPattern(chan);
+}
+
+function spawn(fn, optArgs, optThisArg) {
+  var p = new Process(fn, optArgs, optThisArg);
+  p.step();
+  return p;
 }
 
 module.exports = {
   AsyncChannel: AsyncChannel,
   Channel: Channel,
+  spawn: spawn,
   when: when
 };
--- test/test-joins.js
+++ test/test-joins.js
@@ -4,21 +4,64 @@ var joins = require('..');
 var test = require('tape');
 
 test('simple synchronous send', function (t) {
-  var chan1 = joins.Channel();
+  var chan = joins.Channel();
   var results = [];
 
-  joins.when(chan1).do(function(s) {
+  joins.when(chan).do(function(s) {
     results.push(s);
     return s;
   });
-  chan1.send('Hello, joins!');
+  chan.send('Hello, joins!');
   t.deepEqual(results, ['Hello, joins!']);
 
-  t.equal(chan1.send('another'), 'another');
-  t.equal(chan1.send('and another'), 'and another');
+  t.equal(chan.send('another'), 'another');
+  t.equal(chan.send('and another'), 'and another');
 
   t.equal(results.length, 3);
   t.deepEqual(results, ['Hello, joins!', 'another', 'and another']);
+
+  t.end();
+});
+
+test('simple sync and async combination', function (t) {
+  var chan = joins.Channel();
+  var asyncChan = joins.AsyncChannel();
+
+  joins.when(chan).and(asyncChan).do(function(syncVal, asyncVal) {
+    return syncVal + asyncVal;
+  });
+  asyncChan.send(' async');
+  t.equal(chan.send('Hello'), 'Hello async');
+
+  asyncChan.send(' again!');
+  t.equal(chan.send('Hello'), 'Hello again!');
+
+  asyncChan.send('?');
+  asyncChan.send('!');
+  t.equal(chan.send('wut'), 'wut?');
+  t.equal(chan.send('wut'), 'wut!');
+
+  t.end();
+});
+
+test('blocking synchronous send', function (t) {
+  var chan = joins.Channel();
+  var asyncChan = joins.AsyncChannel();
+
+  joins.when(chan).and(asyncChan).do(function(syncVal, asyncVal) {
+    return syncVal + asyncVal;
+  });
+
+  // Spawn a new process which will block on `chan` until a message is
+  // received on `asyncChan`.
+  var result;
+  var proc = joins.spawn(function*() {
+    result = yield chan.send('Hello');
+  });
+  t.notOk(proc.isComplete(), 'proc is blocking');
+  asyncChan.send(' async');
+  t.ok(proc.isComplete(), 'proc finishes after async send');
+  t.equal(result, 'Hello async');
 
   t.end();
 });
